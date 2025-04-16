61d9b2ff795b2ae1fa6f8e132fc8ba909b19426c
powerllama <powerllama@me.com>
2025-04-16 10:14:34 -0700

    changed my esc keyamp to kj. feels more natural possibly

--- nvim/init.lua
+++ nvim/init.lua
@@ -70,7 +70,7 @@ vim.opt.scrolloff = 10
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set('i', 'jj', '<Esc>')
+vim.keymap.set('i', 'kj', '<Esc>')
 
 -- Diagnostic keymaps
 -- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
